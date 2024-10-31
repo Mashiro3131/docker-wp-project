721ad111d40ccca24bbe2515c17fb82fd5232c92
Nico Mengisen <mengisennico@gmail.com>
2024-10-31 11:46:18 +0100

    chore: Update init-db.sh script to improve MySQL container readiness check and database initialization

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -2,29 +2,29 @@
 
 # Charger les variables d'environnement à partir du fichier .env
 if [ -f .env ]; then
-    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+    export $(grep -v '^#' .env | xargs)
 else
     echo "Fichier .env introuvable. Veuillez en créer un avec les variables d'environnement nécessaires."
     exit 1
 fi
 
-# Attendre que le conteneur MySQL soit prêt
+# Vérifier que le conteneur MySQL est prêt
 echo "En attente de la disponibilité de MySQL..."
 until docker-compose exec mysql mysqladmin ping -h "mysql" --silent; do
     echo -n "."; sleep 1
 done
 echo "MySQL est prêt !"
 
-# Initialiser la base de données
+# Initialisation de la base de données
 echo "Initialisation de la base de données..."
-docker-compose exec mysql mysql -u root -p${MYSQL_ROOT_PASSWORD} <<-EOSQL
+docker-compose exec mysql mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};
     CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';
     GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${WORDPRESS_DB_USER}'@'%';
     FLUSH PRIVILEGES;
 EOSQL
 
-# Fournir un retour à l'utilisateur
+# Retour utilisateur
 if [ $? -eq 0 ]; then
     echo "Initialisation de la base de données réussie !"
 else
